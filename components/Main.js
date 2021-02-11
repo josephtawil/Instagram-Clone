cca7bb767b4412110e5d474b726f9ccd8bc1957a
Joseph <jtawil2015@gmail.com>
2021-02-11 00:12:49 -0800

    fetching data from database using redux

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import {View, Text} from 'react-native';
+//connecting redux 
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchUser} from '../components/redux/actions/index';
 
 export class Main extends Component {
     componentDidMount(){
-        
+        this.props.fetchUser();
     }
     render() {
         return (
@@ -14,4 +18,5 @@ export class Main extends Component {
     }
 }
 
-export default Main
+const mapDispatchToProps = (dispatch) => bindActionCreators({fetchUser}, dispatch);
+export default connect(null, mapDispatchToProps)(Main)
