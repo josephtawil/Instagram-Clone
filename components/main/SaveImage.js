1f64ce318364d3d05a1fafc42788936425d76c40
Joseph <jtawil2015@gmail.com>
2021-02-20 09:51:57 -0800

    created function that saves image uri to firebase storage

--- components/main/SaveImage.js
+++ components/main/SaveImage.js
@@ -1,10 +1,23 @@
 import React,{ useState} from 'react'
 import {SafeAreaView,Button, View, Text, TextInput, Image} from 'react-native';
+import firebase from 'firebase';
+require("firebase/firebase-storage");
 
 export default function SaveImage(props) {
     console.log(props.route.params.image)
 
     const [caption, setCaption] = useState('');
+
+    const uploadImage = async () => {
+        const uri = props.route.params.image;
+        const response = await fetch(uri);
+        const blob = await response.blob();
+
+        const task = firebase
+        .storage()
+        .ref()
+        .child(`post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`)
+    }
 
     return (
         <View style={styles.image}>
@@ -12,7 +25,7 @@ export default function SaveImage(props) {
             <TextInput placeholder="Write a caption ..."
                 onChangeText={(caption)=> setCaption(caption)}
             />
-            <Button title="Save"/>
+            <Button title="Save" onPress={()=> uploadImage()}/>
         </View>
     )
 }
