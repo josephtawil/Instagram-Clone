27befe71469ca1373d1e570dba949e18089e972a
Joseph <jtawil2015@gmail.com>
2021-02-22 21:40:17 -0800

    fixed error

--- components/main/SaveImage.js
+++ components/main/SaveImage.js
@@ -48,7 +48,7 @@ export default function SaveImage(props, {navigation}) {
         .add({
             downloadURL,
             caption, 
-            creation: firebase.firestore().FieldValue.serverTimeStamp()
+            creation: firebase.firestore.FieldValue.serverTimeStamp()
         }).then((function(){
             navigation.popToTop();
         }))
