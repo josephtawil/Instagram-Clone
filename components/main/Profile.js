37fa3f3ed74ef2ff765179e3ed27b0383f2b210d
Joseph <jtawil2015@gmail.com>
2021-02-24 15:10:20 -0800

    used connect react redux to connect the state with the user's profile

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import {SafeAreaView, View, Text} from 'react-native'
-export default function Profile() {
+import {SafeAreaView, View, Text, Image, FlatList} from 'react-native'
+import {connect} from 'react-redux'
+
+ function Profile(props) {
+     const {currentUser, posts} = props;
+     console.log(currentUser, posts)
     return (
         <SafeAreaView>
-            <Text>Profile Page</Text>
+            <Text>currentUser</Text>
         </SafeAreaView>
     )
 }
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    posts: store.userState.posts
+})
+
+export default connect(mapStateToProps, null)(Profile);
